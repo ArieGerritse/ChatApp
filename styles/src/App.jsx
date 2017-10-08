4fdbc628f93c66dc375e7f01015b0c1524984ae9
Arie Gerritse <gerritse.arie@gmail.com>
2017-10-08 11:33:19 -0600

    Broke Everything

---
+++ styles/src/App.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
+// const server = new WebSocket("ws:localhost:3001");
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.newMessage = this.newMessage.bind(this);
+    // this.sendNotification = this.sendNotification.bind(this);
+    this.state = {
+      connect: { server: new WebSocket('ws:localhost:3001') },
+      currentUser: { name: 'Anonymous' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [],
+      onlineUsers: 1,
+      color: 'black'
+    };
+  }
+
+  componentDidMount() {
+    this.state.connect.server.addEventListener('message', event => {
+      let newMessage = JSON.parse(event.data);
+      if (newMessage.type === 'numbersOnline') {
+        this.setState({ onlineUsers: newMessage.online });
+      } else {
+        const newMessages = this.state.messages.concat(newMessage);
+        this.setState({ messages: newMessages });
+      }
+    });
+  }
+
+  // sendNotification(newUsername, oldUsername){
+  //   let newNotification = {type: "postNotification", username: newUsername, oldUsername: oldUsername};
+  //   this.state.connect.server.send(JSON.stringify(newNotification));
+  // }
+
+  newMessage(newUsername, message) {
+    let newMessage = {};
+    if (newUsername === this.state.currentUser.name || newUsername === '') {
+      newMessage = {
+        type: 'postMessage',
+        username: this.state.currentUser.name,
+        content: message,
+        color: this.state.color
+      };
+    } else {
+      this.setState({ color: getRandomColor() });
+      let newNotification = {
+        type: 'postNotification',
+        username: newUsername,
+        oldUsername: this.state.currentUser.name
+      };
+      this.state.connect.server.send(JSON.stringify(newNotification));
+      newMessage = {
+        type: 'postMessage',
+        username: newUsername,
+        content: message,
+        color: this.state.color
+      };
+      this.setState({ currentUser: { name: newUsername } });
+    }
+    this.state.connect.server.send(JSON.stringify(newMessage));
+  }
+
+  render() {
+    return (
+      <div className="Chatty">
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">
+            Chatty
+          </a>
+          <span className="online">Online {this.state.onlineUsers}</span>
+        </nav>
+        <MessageList allMessages={this.state} />
+        <ChatBar
+          newMessage={this.newMessage}
+          currentUser={this.state.currentUser.name}
+        />
+      </div>
+    );
+  }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+export default App;
---
+++ styles/src/MessageList.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Message from "./Message.jsx";
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const messages = this.props.allMessages.messages.map(messages => {
+      switch (messages.type) {
+        case "incomingMessage":
+          return (
+            <Message
+              key={messages.id}
+              username={messages.username}
+              content={messages.content}
+              color={{ color: messages.color }}
+            />
+          );
+        case "incomingNotification":
+          return (
+            <Message
+              key={messages.id}
+              content={`${messages.oldUsername} has changed thier name to ${messages.username}`}
+            />
+          );
+      }
+    });
+
+    return <div className="messages">{messages}</div>;
+  }
+}
+
+export default MessageList;
